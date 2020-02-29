2c4169a8efc2a5af7135f4234cf9adf4834c6f89
disen-xaqf <610039018@qq.com>
2020-02-29 16:50:35 +0800

    修复模板

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -15,11 +15,7 @@ class LoginMiddleware(MiddlewareMixin):
     )
 
     def process_request(self, request: HttpRequest):
-
-        if request.path not in self.no_filter_path or \
-                not request.path.startswith('/s/') or \
-                not request.path.startswith('/m/'):
-
+        if request.path not in self.no_filter_path:
             # 验证当前会话是否已登录
             if not request.session.get('login_user', None):
                 return redirect('/login/')
